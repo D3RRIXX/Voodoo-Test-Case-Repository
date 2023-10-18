576d7d1599a1a9bb8b9734c33a71bf5eab79cc59
D3RRIXX <jenyazagevskii@gmail.com>
2023-10-18 09:09:06 +0300

    Extracted follow camera logic

---
+++ Voodoo Case/Assets/_Space Rails_/Scripts/FollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SpaceRails.Infrastructure;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using Zenject;
+
+namespace SpaceRails
+{
+	public class FollowCamera : MonoBehaviour
+	{
+		[SerializeField] private Transform _target;
+
+		private Vector3 _offset;
+
+		[Inject]
+		private void Construct(GameStateManager gameStateManager)
+		{
+			_offset = transform.position - _target.position;
+
+			this.LateUpdateAsObservable()
+			    .TakeUntil(gameStateManager.CurrentState.SkipLatestValueOnSubscribe().First(x => x is GameState.LevelComplete or GameState.LevelFailed))
+			    .Subscribe(_ => transform.position = _target.position + _offset);
+		}
+	}
+}
